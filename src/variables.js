98648c8ababd0d8133fb3e52c01bcd9c678b0414
JonathanPereira7 <jona714247@gmail.com>
2023-05-15 22:48:11 -0300

    change json to local array in useProducts

--- src/variables.js
+++ src/variables.js
@@ -4,92 +4,121 @@ export const API_PRODUCTS_URL = `https://api.escuelajs.co/api/v1/products`
 export const PRODUCTS = [
   {
     id: 1,
-    title: 'Laptop Lenovo',
+    title: "Laptop Lenovo",
     price: 35000,
-    description: 'Laptop Lenovo 16", FHD, 16GB, 1TB solid, 65Hz',
-    category: 'laptop',
-    images: ['laptop', 'laptop2'],
+    description: "Laptop Lenovo 16', FHD, 16GB, 1TB solid, 65Hz",
+    category: "laptop",
+    images: [
+      "laptop",
+      "laptop2"
+    ],
     favorite: false
   },
   {
     id: 2,
-    title: 'Laptop HP',
+    title: "Laptop HP",
     price: 25000,
-    description: 'Laptop HP 14", FHD, 8GB, 512MB solid, 65Hz',
-    category: 'laptop',
-    images: ['laptop2', 'laptop'],
+    description: "Laptop HP 14', FHD, 8GB, 512MB solid, 65Hz",
+    category: "laptop",
+    images: [
+      "laptop2",
+      "laptop"
+    ],
     favorite: false
   },
   {
     id: 3,
-    title: 'Computadora Gamer',
+    title: "Computadora Gamer",
     price: 60000,
-    description: 'Computadora Gamer, 4GB GeForce 3060x, 16GB, 1TB solid, Razen 5800',
-    category: 'computadora',
-    images: ['computadora', 'computadora2'],
+    description: "Computadora Gamer, 4GB GeForce 3060x, 16GB, 1TB solid, Razen 5800",
+    category: "computers",
+    images: [
+      "computadora",
+      "computadora2"
+    ],
     favorite: false
   },
   {
     id: 4,
-    title: 'Laptop Lenovo Nueva',
+    title: "Laptop Lenovo Nueva",
     price: 55000,
-    description: 'Laptop Lenovo 17", FHD, 32GB, 1TB solid, 165Hz',
-    category: 'laptop',
-    images: ['laptop', 'laptop2'],
+    description: "Laptop Lenovo 17', FHD, 32GB, 1TB solid, 165Hz",
+    category: "laptop",
+    images: [
+      "laptop",
+      "laptop2"
+    ],
     favorite: false
   },
   {
     id: 5,
-    title: 'Auriculares Orejita',
+    title: "Auriculares Orejita",
     price: 12000,
-    description: 'Auriculares con orejitas para verte super cool cuando programas o perdes unas partidas de LoL',
-    category: 'auriculares',
-    images: ['auriculares2'],
+    description: "Auriculares con orejitas para verte super cool cuando programas o perdes unas partidas de LoL",
+    category: "accesories",
+    images: [
+      "auriculares2"
+    ],
     favorite: false
   },
   {
     id: 6,
-    title: 'Auriculares Gamer',
+    title: "Auriculares Gamer",
     price: 15000,
-    description: 'Auriculares Gamer mega pro',
-    category: 'auriculares',
-    images: ['auriculares', 'auriculares2'],
+    description: "Auriculares Gamer mega pro",
+    category: "accesories",
+    images: [
+      "auriculares",
+      "auriculares2"
+    ],
     favorite: false
   },
   {
     id: 7,
-    title: 'Monitor Samsung',
+    title: "Monitor Samsung",
     price: 25000,
-    description: 'Monitor Samsung UFHD, 27", 1800R, re gigante para poder jugar unas partiditas de LoL',
-    category: 'monitor',
-    images: ['monitor', 'monitor2'],
+    description: "Monitor Samsung UFHD, 27', 1800R, re gigante para poder jugar unas partiditas de LoL",
+    category: "monitor",
+    images: [
+      "monitor",
+      "monitor2"
+    ],
     favorite: false
   },
   {
     id: 8,
-    title: 'Monitor Gamer',
+    title: "Monitor Gamer",
     price: 35000,
-    description: 'Monitor Samsung 4K, 24", 1800R, re gigante para poder jugar unas partiditas de LoL y Valorant',
-    category: 'monitor',
-    images: ['monitor2', 'monitor'],
-    favorite: true
+    description: "Monitor Samsung 4K, 24', 1800R, re gigante para poder jugar unas partiditas de LoL y Valorant",
+    category: "monitor",
+    images: [
+      "monitor2",
+      "monitor"
+    ],
+    favorite: false
   },
   {
     id: 9,
-    title: 'Monitor Samsung',
+    title: "Monitor Samsung",
     price: 25000,
-    description: 'Monitor Samsung UFHD, 27", 1800R, re gigante para poder jugar unas partiditas de LoL',
-    category: 'monitor',
-    images: ['monitor', 'monitor2'],
-    favorite: true
+    description: "Monitor Samsung UFHD, 27', 1800R, re gigante para poder jugar unas partiditas de LoL",
+    category: "monitor",
+    images: [
+      "monitor",
+      "monitor2"
+    ],
+    favorite: false
   },
   {
     id: 10,
-    title: 'Computadora de escritorio',
+    title: "Computadora",
     price: 75000,
-    description: 'Intel core I9 9800, 32GB, 16GB 3060ti, 512GB solid, 1TB hard',
-    category: 'computadora',
-    images: ['computadora', 'computadora2'],
+    description: "Intel core I9 9800, 32GB, 16GB 3060ti, 512GB solid, 1TB hard",
+    category: "computers",
+    images: [
+      "computadora",
+      "computadora2"
+    ],
     favorite: true
   }
 ]
