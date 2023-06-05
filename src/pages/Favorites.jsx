26183fc0cb27d4c59a18290c9937dd96e9a88c5d
JonathanPereira7 <jona714247@gmail.com>
2023-06-05 18:50:22 -0300

    BtnBack added in favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,8 +1,10 @@
+import { BtnBack } from '../elements/buttons/BtnBack';
 
 export function Favorites () {
   return (
     <main style={{minHeight: '70vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
       <span>work in progres..</span>
+      <BtnBack path={'/'} text='Go Back'/>
     </main>
   )
 }
